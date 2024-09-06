49db566a425a760e40913e1baa132f646f59ea5b
sagarch <sagar192010@gmail.com>
2024-09-06 18:39:31 +0530

    Todo-added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import Accordian from "./components/Accordian";
 import Todo2 from "./components/Todo2";
 
 function App() {
 	return (
 		<div className="flex flex-col justify-center align-middle text-center font-mono">
 			<Todo2 />
-			<Accordian />
 		</div>
 	);
 }
